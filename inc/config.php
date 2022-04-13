e38ae9e9d4420510922cb64514ca6c476c11d386
tzbbq <103579424+TZlovewall@users.noreply.github.com>
2022-04-13 20:52:35 +0800

    Add files via upload

---
+++ inc/config.php
@@ -0,0 +1,12 @@
+<?php
+
+
+
+	$mysql_server_name='localhost'; //Mysql数据库服务器
+	
+	$mysql_username='cesi'; //Mysql数据库用户名
+	
+	$mysql_password='1234444'; //Mysql数据库密码
+	
+	$mysql_database='cesi'; //Mysql数据库名
+?>
